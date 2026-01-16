08d1026941fde3a844470d6eaa008e9fe5e1facf
Peter Garbett <petegarbett1958@gmail.com>
2026-01-16 22:44:50 +0000

    Add charging enable/disable scripts

---
+++ disable_charging.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# As defined in 
+# https://suptronics.com/Raspberrypi/Power_mgmt/x120x-v1.0_software.html
+
+pinctrl set 16 op dh
+
+
+	 
---
+++ enable_charging.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# As defined by
+# https://suptronics.com/Raspberrypi/Power_mgmt/x120x-v1.0_software.html
+
+pinctrl set 16 op dl
